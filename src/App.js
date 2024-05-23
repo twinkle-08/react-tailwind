a4d01d602ed080367a88f1067cfcc00ed2451388
twinkle-08 <aec.cse.twinklesingh@gmail.com>
2024-05-23 18:06:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import {useState} from 'react';
+
+function App() {
+  
+  const[count,setCount]=useState(0);
+  function decreaseHandler()
+  {
+    setCount(count-1);
+  }
+  function increaseHandler()
+  {
+    setCount(count+1);
+  }
+  function resetHandler(){
+    setCount(0);
+  }
+  return(
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl font-medium">Increment and Decrement</div>
+      <div className="bg-white flex flex-row justify-center font-medium  py-3 rounded-sm text-[25px] gap-20 text-[#344151]">
+        <button onClick={decreaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf]">
+          -
+        </button>
+        <div className="font-bold gap-12 text-5xl">   
+            {count}
+        </div>
+        <button onClick={increaseHandler} className="border-l-2 text-center w-20 border-[#bfbfbf] ">
+          +
+        </button>
+      </div>
+      <button onClick={resetHandler} className="bg-[#0398d4] px-5 py-2 rounded-sm text-white font-medium text-lg">
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default App;
